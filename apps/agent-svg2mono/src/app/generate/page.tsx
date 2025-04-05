5dcff2f118aec7faa4a9338603f34d1110dc13a4
Ronny Badilla <info@ronnybadilla.com>
2025-04-04 19:11:57 -0600

    feat: added hooks

--- apps/agent-svg2mono/src/app/generate/page.tsx
+++ apps/agent-svg2mono/src/app/generate/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import { useSvgMonochrome } from '@/hooks/use-svg-monochrome';
+import { useSvgMonochrome } from '@/registry/new-york/use-svg-monochrome/use-svg-monochrome';
 
 interface PreviewProps {
   svg: string | null;
